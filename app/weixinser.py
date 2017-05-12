000ba22b0e57db6008d2a31427594c632b890c33
ngerll <ngerll@yeah.net>
2017-05-12 23:46:50 +0800

    Update weixinser.py

--- app/weixinser.py
+++ app/weixinser.py
@@ -14,26 +14,27 @@ robot = werobot.WeRoBot(token='ngerllTower')
 
 @robot.handler
 def echo(message):
-    #db = MySQLdb.connect(host='220.249.117.234', user='root', passwd='!q2w3e4r', db='wxauto', port=13306,
-    #                     charset='utf8')
-    #cursor = db.cursor()
+    db = MySQLdb.connect(host='61.183.198.35', user='root', passwd='!q2w3e4r', db='wxauto', port=13306,
+                         charset='utf8')
+    cursor = db.cursor()
 
-    #if message.content == 'sp':
-    #    pass
-    #else:
-    try:
-            #sql = "select * from userinfo where user_id = '%s' " % message.content
-            #cursor.execute(sql)
-            #resinfo = cursor.fetchone()
+    if message.content == 'sp':
+        pass
+    else:
+        try:
+            sql = "select * from userinfo where user_id = '%s' " % message.content
+            cursor.execute(sql)
+            resinfo = cursor.fetchone()
+            
+            userid = resinfo[0]
+            passwd = resinfo[1]
+            
+            res = getproblemPortal.getprolist(userid, passwd)  
+            
+            return res
 
-            #userid = resinfo[0]
-            #passwd = resinfo[1]
-
-        res = getproblemPortal.getprolist('huqiao', 'valencia429')
-        return res
-
-    except:
-        return '错误,请重试!'
+        except:
+            return '错误,请重试!'
 
 
 
